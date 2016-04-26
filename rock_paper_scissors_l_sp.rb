d987ab9b12c404569693dfb5fa4b117e7d3c3570
brianweller <brmweller@gmail.com>
2016-04-26 11:46:19 -0700

    Added files via upload

--- rock_paper_scissors_l_sp.rb
+++ rock_paper_scissors_l_sp.rb
@@ -78,11 +78,8 @@ while user_score < 5 && comp_score < 5
     say(prompt_to_user)
     choice = Kernel.gets.chomp.downcase
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      say "That's not valid"
-    end
+    break if VALID_CHOICES.include?(choice)
+    say "That's not valid"
   end
 
   # get comp random choice
@@ -104,17 +101,11 @@ while user_score < 5 && comp_score < 5
   say "Your score: #{user_score}."
   say "The computer's score: #{comp_score}."
 
-  if user_score == 4 && comp_score == 4
-    say '-----SUDDEN DEATH-----'
-  end
+  say '-----SUDDEN DEATH-----' if user_score == 4 && comp_score == 4
 
-  if user_score == 4
-    say 'One more round win and you WIN...'
-  end
+  say 'One more round win and you WIN...' if user_score == 4
 
-  if comp_score == 4
-    say 'The computer only needs one more round win to WIN...'
-  end
+  say 'The computer only needs one more round win to WIN...' if comp_score == 4
 
   clear_screen
 
@@ -122,7 +113,7 @@ end
 
 if user_score == 5
   say 'Congrats, you won the Game!'
-  say 'The score was #{user_score} to #{comp_score}.'
+  say "The score was #{user_score} to #{comp_score}."
 else
   say 'The computer won the game...'
   say "The score was #{comp_score} to #{user_score}. Maybe next time!"
